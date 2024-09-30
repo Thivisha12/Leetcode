ca19a8d1716ec41f13f13b1fae2f96ebf9c92f36
THIVISHA SENTHILNATHAN <126443377+Thivisha12@users.noreply.github.com>
2024-09-30 16:07:03 +0530

    Time: 11 ms (21.26%), Space: 15.8 MB (27.54%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.c
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.c
@@ -23,19 +23,19 @@
 //     else
 //         return ConvToBST(nums, 0, numsSize-1);
 // }
-struct TreeNode* ConvtoBST(int *nums,int beg,int end){
+struct TreeNode* convtoBST(int *nums,int beg,int end){
     if(end<beg)
      return NULL;
     int mid=(beg+end)/2;
     struct TreeNode* root=(struct TreeNode*)malloc(sizeof(struct TreeNode));
     root->val=nums[mid];
-    root->left=ConvtoBST(nums,beg,mid-1);
-    root->right=ConvtoBST(nums,mid+1,end);
+    root->left=convtoBST(nums,beg,mid-1);
+    root->right=convtoBST(nums,mid+1,end);
     return root;
  }
  struct TreeNode* sortedArrayToBST(int *nums,int numsSize){
     if(numsSize<=0)
      return NULL;
     else
-     return ConvtoBST(nums,0,numsSize-1);
+     return convtoBST(nums,0,numsSize-1);
  }
