5577aabbe72408310e4311aad7c2c12bf4d5104b
THIVISHA SENTHILNATHAN <126443377+Thivisha12@users.noreply.github.com>
2024-10-07 17:26:17 +0530

    Time: 1 ms (99.55%), Space: 43.2 MB (97.07%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,24 +1,46 @@
 
 class Solution {
     public int compress(char[] chars) {
-        int n = chars.length;
-        int i = 0;
-        int idx = 0;
-        while (i < n) {
-            int c=0;
-            char ch = chars[i];
-            while (i < n && chars[i] == ch){
-                c++;
-                i++;
-            }
-            chars[idx++] = ch;
-           if(c>1){
+        // int n = chars.length;
+        // int i = 0;
+        // int idx = 0;
+        // while (i < n) {
+        //     int c=0;
+        //     char ch = chars[i];
+        //     while (i < n && chars[i] == ch){
+        //         c++;
+        //         i++;
+        //     }
+        //     chars[idx++] = ch;
+        //    if(c>1){
                 
-                String s=String.valueOf(c);
-                for (char chh : s.toCharArray())
-                    chars[idx++] = chh;
-            }
+        //         String s=String.valueOf(c);
+        //         for (char chh : s.toCharArray())
+        //             chars[idx++] = chh;
+        //     }
+        // }
+        // return idx;
+        int n=chars.length;
+        int i=0;
+        int idx=0;
+       while(i<n)
+       {
+        int c=0;
+        char ch=chars[i];
+        while(i<n && chars[i]==ch)
+        {
+            c++;
+            i++;
+
         }
-        return idx;
+        chars[idx++]=ch;
+        if(c>1)
+        {
+             String s=String.valueOf(c);
+        for(char chh : s.toCharArray())
+        chars[idx++]=chh;
+        }
+       }
+       return idx;
     }
 }
