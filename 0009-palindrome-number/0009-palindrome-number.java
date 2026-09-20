30b6f698e0a04838c5c72ca36d98580e82bea0d8
THIVISHA SENTHILNATHAN <thivishasendhilnathan@gmail.com>
2026-09-20 14:17:18 +0530

    Time: 5 ms (83.36%), Space: 45.9 MB (52.82%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int original=x;
+        int rev=0;
+        while(x>0){
+            int d=x%10;
+            rev=rev*10+d;
+            x=x/10;
+        }
+        if(rev==original){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
